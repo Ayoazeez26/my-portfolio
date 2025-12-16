0f411a76648be2d1da03b82cf54c05451d1d9bda
Ayoazeez26 <44169294+Ayoazeez26@users.noreply.github.com>
2025-12-16 13:28:58 +0100

    feat: attempt to fix seo meta images

--- app/app.vue
+++ app/app.vue
@@ -19,8 +19,8 @@ useHead({
 
 useSeoMeta({
   titleTemplate: "%s - Ayoazeez26 Portfolio",
-  ogImage: "",
-  twitterImage: "",
+  ogImage: "https://my-hakeem-portfolio.vercel.app/og-hero-image.png",
+  twitterImage: "https://my-hakeem-portfolio.vercel.app/og-hero-image.png",
   twitterCard: "summary_large_image",
 });
 
